88a06b6c18fb0f90b96a3907c7f32c7f8b618ffc
Diego Jahn <jahndiego@mailbox.org>
2019-08-12 01:06:38 +0200

    Adjust Pin numbers.

--- e-ink_display/epd_simpletest/code.py
+++ e-ink_display/epd_simpletest/code.py
@@ -10,11 +10,11 @@ from adafruit_epd.ssd1675 import Adafruit_SSD1675  # pylint: disable=unused-impo
 
 # create the spi device and pins we will need
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-ecs = digitalio.DigitalInOut(board.D12)
-dc = digitalio.DigitalInOut(board.D11)
-srcs = digitalio.DigitalInOut(board.D10)    # can be None to use internal memory
-rst = digitalio.DigitalInOut(board.D9)    # can be None to not use this pin
-busy = digitalio.DigitalInOut(board.D5)   # can be None to not use this pin
+ecs = digitalio.DigitalInOut(board.D9)
+dc = digitalio.DigitalInOut(board.D10)
+srcs = digitalio.DigitalInOut(board.D6)    # can be None to use internal memory
+rst = None #digitalio.DigitalInOut(board.D9)    # can be None to not use this pin
+busy = None #digitalio.DigitalInOut(board.D5)   # can be None to not use this pin
 
 # give them all to our driver
 print("Creating display")
